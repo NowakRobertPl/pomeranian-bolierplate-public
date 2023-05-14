58a0f9befbb825cc68b26a00749ed296178f3187
RobertNowak <nowakrobertpl@gmail.com>
2023-05-14 16:34:33 +0200

    zad-tabele-nr1

---
+++ src/App/Exercises/HtmlCss/HTMLTables/router-data.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import {Tables} from './HTMLTables';
+
+export const blockRouterMetaData = {
+  path: '14-05-2023-1',
+  date: '12-05-2023',
+  linkLabel: 'Tabele',
+  blockNo: 15,
+  element: <Tables />,
+tags: ['tables', 'HTML', 'css'],
+};
