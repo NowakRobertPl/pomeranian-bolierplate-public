135c65b6cea13bd54120c956583b90980bfd5fc4
RobertNowak <nowakrobertpl@gmail.com>
2023-05-21 13:39:39 +0200

    JS booleans

---
+++ src/App/Exercises/Js/Exercise-js-numbers/index.jsx
@@ -0,0 +1,12 @@
+import './styles.css';
+
+export function Exercise() {
+  // Math.round()
+  // Math.floor()
+  // Math.random()
+  // Math.min()
+  // Math.max()
+  // Math.ceil()
+
+  return <>test</>;
+}
---
+++ src/App/Exercises/Js/Exercise-js-numbers/router-data.jsx
@@ -0,0 +1,10 @@
+import { Exercise } from './index';
+
+export const blockRouterMetaData = {
+  path: 'js-numbers',
+  date: null,
+  linkLabel: 'Numbers w JS',
+  blockNo: 19,
+  element: <Exercise />,
+  tags: ['numbers', 'JS', 'int', 'float', 'const'],
+};
