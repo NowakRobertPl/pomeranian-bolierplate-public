48ebd030e4d7a260c18755b47855962c69ac4ce4
RobertNowak <nowakrobertpl@gmail.com>
2023-05-21 13:52:28 +0200

    liczby i logika w JS

---
+++ src/App/Exercises/Js/JsNumbers/router-data.jsx
@@ -0,0 +1,10 @@
+import { canDrive } from './index';
+
+export const blockRouterMetaData = {
+  path: 'js-numbers-training',
+  // date: null,
+  linkLabel: 'Liczby i booleany w JS',
+  blockNo: 19,
+  element: <canDrive />,
+  tags: ['numbers', 'JS', 'bool', 'booleans'],
+};
